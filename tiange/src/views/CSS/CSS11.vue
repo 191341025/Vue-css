6eeb43622baa028f07c52caf789db6408ee7fc43
senxu.tian <191341025@qq.com>
2020-05-24 13:59:03 +0800

    CSS11 页面加载等待例子 优化

--- tiange/src/views/CSS/CSS11.vue
+++ tiange/src/views/CSS/CSS11.vue
@@ -31,64 +31,25 @@
   .loading-box div
     position absolute
     border-radius 50%
-    animation loading 2s infinite
+    animation loading 2s infinite ease-in-out
     opacity 0
   .loading-box div:nth-child(1)
     width 100%
     height 100%
     background-color rgba(18, 129, 52, 1)
-    animation-delay 1s
+    animation-delay -1s
   .loading-box div:nth-child(2)
     width 100%
     height 100%
     background-color rgba(18, 129, 52, 1)
 
-
-
   @keyframes loading
-    0%{
-      transform scale(1)
+    0%,100%{
+      transform scale(0.1)
       opacity 0.1
-    }
-    10%{
-      transform scale(0.8)
-      opacity 0.2
-    }
-    20%{
-      transform scale(0.6)
-      opacity 0.4
-
-    }
-    30%{
-      transform scale(0.4)
-      opacity 0.6
-    }
-    40%{
-      transform scale(0.2)
-      opacity 0.8
     }
     50%{
-      transform scale(0)
-      opacity 1
-    }
-    60%{
-      transform scale(0.2)
-      opacity 0.8
-    }
-    70%{
-      transform scale(0.4)
-      opacity 0.6
-    }
-    80%{
-      transform scale(0.6)
-      opacity 0.4
-    }
-    90%{
-      transform scale(0.8)
-      opacity 0.2
-    }
-    100%{
       transform scale(1)
-      opacity 0.1
+      opacity 0.5
     }
 </style>
